3c331437fba84c4a60b7d78abfd14cdc49a1e54f
qaeu <43359569+qaeu@users.noreply.github.com>
2025-11-22 17:08:10 +0000

    Update default region profile for refactor

--- src/data/profiles/index.ts
+++ src/data/profiles/index.ts
@@ -1,8 +1,9 @@
-import { ExportedProfile } from '#utils/regionProfiles.js';
+import type { ExportedProfile } from '#types';
 import postGame5v5 from './post-game_5v5.json';
 
 /**
  * Default region profiles bundled with the app
  * Each profile should have type: 'vowt-region-profile' to be loaded
  */
-export const defaultProfiles = [postGame5v5 as ExportedProfile];
+
+export const DEFAULT_PROFILES = [postGame5v5 as ExportedProfile];
