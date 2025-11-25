9a6f943a625a5bbdebcd9e09eb8379360eb90d05
qaeu <43359569+qaeu@users.noreply.github.com>
2025-11-25 03:33:32 +0000

    Add default profile for in-game scoreboard screenshots

--- src/data/profiles/index.ts
+++ src/data/profiles/index.ts
@@ -1,9 +1,13 @@
 import type { ExportedProfile } from '#types';
 import postGame5v5 from './post-game_5v5.json';
+import inGame5v5 from './in-game_5v5.json';
 
 /**
  * Default region profiles bundled with the app
  * Each profile should have type: 'vowt-region-profile' to be loaded
  */
 
-export const DEFAULT_PROFILES = [postGame5v5 as ExportedProfile];
+export const DEFAULT_PROFILES = [
+	postGame5v5 as ExportedProfile,
+	inGame5v5 as ExportedProfile,
+];
