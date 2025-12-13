4f7e9be034f30a16b04d24808b84c60d7c58f913
qaeu <43359569+qaeu@users.noreply.github.com>
2025-12-13 01:31:48 +0000

    Create Toaster UI component

---
+++ src/components/ui/Toaster.tsx
@@ -0,0 +1,39 @@
+import type { Component } from 'solid-js';
+import { Toast, Toaster as ArkToaster, createToaster } from '@ark-ui/solid/toast';
+
+const toaster = createToaster({
+	placement: 'bottom-end',
+	gap: 46,
+	overlap: true,
+	offsets: '15px',
+	duration: 60000,
+});
+
+export const toast = (title: string, description: string) => {
+	toaster.create({
+		title,
+		description,
+	});
+};
+
+interface ToasterProps {
+	class?: string;
+}
+
+const Toaster: Component<ToasterProps> = (props) => {
+	return (
+		<ArkToaster toaster={toaster}>
+			{(toast) => (
+				<Toast.Root class={props.class}>
+					<Toast.Title asChild={(props) => <h3 {...props()} />}>
+						{toast().title}
+					</Toast.Title>
+					<Toast.Description>{toast().description}</Toast.Description>
+					<Toast.CloseTrigger>✕</Toast.CloseTrigger>
+				</Toast.Root>
+			)}
+		</ArkToaster>
+	);
+};
+
+export default Toaster;
